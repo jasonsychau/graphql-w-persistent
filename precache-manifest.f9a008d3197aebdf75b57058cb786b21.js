5a10b3495796813072da1d45abecfc4ac2d4acf9
jason <jasonsychau@live.ca>
2019-12-20 22:02:30 -0800

    add web page

---
+++ precache-manifest.f9a008d3197aebdf75b57058cb786b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82a534d53b2fbb47fe4970651d50fb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05279cf57d619feca115",
+    "url": "./static/css/main.b4f892a5.chunk.css"
+  },
+  {
+    "revision": "78e2865598420d28f9c4",
+    "url": "./static/js/2.74eaa836.chunk.js"
+  },
+  {
+    "revision": "05279cf57d619feca115",
+    "url": "./static/js/main.e95cf619.chunk.js"
+  },
+  {
+    "revision": "e9eb79e6a7a85e5029db",
+    "url": "./static/js/runtime-main.e5beeb76.js"
+  },
+  {
+    "revision": "418c4552054a9e0b8ae47e285ef52b43",
+    "url": "./static/media/FC_Smaller-Glow.418c4552.png"
+  },
+  {
+    "revision": "efb61565da0a750340fa6d0122c55640",
+    "url": "./static/media/FC_Smaller.efb61565.png"
+  }
+]);
